9f54e043b0dc187dc2a51d60ccb8c5657e27743d
Luiz Carlos <luizcssoares@gmail.com.com>
2025-07-28 23:34:00 -0300

    Alteracao...

--- src/main/java/com/operacao/apirestcalculadora/ApirestcalculadoraApplication.java
+++ src/main/java/com/operacao/apirestcalculadora/ApirestcalculadoraApplication.java
@@ -2,12 +2,9 @@ package com.operacao.apirestcalculadora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApirestcalculadoraApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ApirestcalculadoraApplication.class, args);
-	}
+public class ApirestcalculadoraApplication extends SpringBootServletInitializer {
 
 }
