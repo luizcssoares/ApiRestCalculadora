2a8f581b1d493038368e27c51ffa29e02fab6db1
Luiz Carlos <luizcssoares@gmail.com.com>
2025-07-31 20:47:02 -0300

    Mais Ajustes

--- src/main/java/com/operacao/apirestcalculadora/ApirestcalculadoraApplication.java
+++ src/main/java/com/operacao/apirestcalculadora/ApirestcalculadoraApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApirestcalculadoraApplication  extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
@@ -13,5 +16,9 @@ public class ApirestcalculadoraApplication  extends SpringBootServletInitializer
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(ApirestcalculadoraApplication.class, args);
+	}
+	@RequestMapping("/")
+	private String Soma(){
+		return "Soma e 20";
 	}
 }
