d77c7bc45556aef99a5417122e43f8c41ab9d444
Luiz Carlos Soares <luizcssoares@gmail.com>
2025-09-05 12:03:33 -0300

    Ajuste arquivo principal

--- src/main/java/com/operacao/apirestcalculadora/ApirestcalculadoraApplication.java
+++ src/main/java/com/operacao/apirestcalculadora/ApirestcalculadoraApplication.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class ApirestcalculadoraApplication  extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
